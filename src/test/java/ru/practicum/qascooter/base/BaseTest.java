c81b5825da11ed0f72478b6e9bd9b91654ba3988
Yuliya Valiulina <kjs060994@gmail.com>
2025-07-21 17:57:29 +0700

    Тест для браузера хром и файерфокс

--- src/test/java/ru/practicum/qascooter/base/BaseTest.java
+++ src/test/java/ru/practicum/qascooter/base/BaseTest.java
@@ -1,24 +1,52 @@
 package ru.practicum.qascooter.base;
 
-import static org.junit.Assert.*;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import ru.practicum.qascooter.pages.HomePage;
 import org.openqa.selenium.WebDriver;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.Collection;
 
+@RunWith(Parameterized.class)
 public class BaseTest {
-    protected static WebDriver driver;
-    protected static HomePage homePage;
+    protected WebDriver driver;
+    protected HomePage homePage;
+    private final String browser;
 
-    @BeforeClass
-    public static void setUp() {
-        System.out.println("setUp() вызван");
-        ChromeOptions options = new ChromeOptions();
-        driver = new ChromeDriver(options);
+    public BaseTest(String browser) {
+        this.browser = browser;
+    }
+
+    @Parameterized.Parameters(name = "Запуск в браузере: {0}")
+    public static Collection<String> browsers() {
+        return Arrays.asList("chrome", "firefox");
+    }
+
+    @Before
+    public void setUp() {
+        System.out.println("setUp() вызван для браузера " + browser);
+
+        if (browser.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            driver = new ChromeDriver(options);
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            FirefoxOptions options = new FirefoxOptions();
+            driver = new FirefoxDriver(options);
+        }
+
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         homePage = new HomePage(driver);
         homePage.open();
@@ -26,8 +54,8 @@ public class BaseTest {
         homePage.clickCookieButton();
     }
 
-    @AfterClass
-    public static void tearDown() {
+    @After
+    public void tearDown() {
         if (driver != null) {
             driver.quit();
         }
